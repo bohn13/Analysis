5c8e89d5558d5502ef86a494ffffe93a99ec04f9
bohn13 <bod3171@gmail.com>
2025-06-12 02:14:56 +0200

    Again changes for deployment

--- ecommerce-nextjs/next.config.ts
+++ ecommerce-nextjs/next.config.ts
@@ -1,9 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ['picsum.photos'],
-  },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
 };
 
 export default nextConfig;
